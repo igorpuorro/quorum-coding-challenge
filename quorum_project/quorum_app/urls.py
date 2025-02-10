8956fc1f41a356272c2ae708a616fd982d92c600
Igor Puorro <igor.anfp@gmail.com>
2025-02-10 01:02:19 -0300

    feat(quorum_app): add API documentation and authentication
    - Integrate `drf_spectacular` for OpenAPI schema generation and Swagger UI
    - Add Token-based authentication

--- quorum_project/quorum_app/urls.py
+++ quorum_project/quorum_app/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
 
 from .views import BillSummaryView, BillViewSet, PersonViewSet, LegislatorSummaryView, VoteViewSet, VoteResultViewSet
@@ -13,6 +14,12 @@ router.register(prefix=r"vote", viewset=VoteViewSet)
 router.register(prefix=r"vote-result", viewset=VoteResultViewSet)
 
 urlpatterns = [
+    path(
+        "api/schema/", SpectacularAPIView.as_view(), name="schema"
+    ),
+    path(
+        "api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"
+    ),
     path(
         "api/v1/", include(router.urls)
     ),
--- quorum_project/quorum_project/settings.py
+++ quorum_project/quorum_project/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_spectacular',
     'rest_framework',
+    'rest_framework.authtoken',
     'quorum_app',
     'quorum_app_ui',
 ]
@@ -124,3 +126,35 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Quorum Coding Challenge API',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': True,
+    'SWAGGER_UI_SETTINGS': {
+        'deepLinking': True,
+        'persistAuthorization': True,
+    },
+    'SECURITY': [{
+        'Token': [],
+    }],
+    'SECURITY_DEFINITIONS': {
+        'Token': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header',
+        },
+    },
+}
